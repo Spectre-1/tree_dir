45e919efc77b846b06fc9bb3ffbcfa9be9e5417e
Spectre <pgudijan@uci.edu>
2023-05-25 01:41:49 -0700

    Finalized header and footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import '../tailwind.css';
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 right-0 h-16 w-16 bg-gray-200">
-      <div className="container mx-auto py-4 flex items-center justify-center">
-        <p className="text-gray-600">
-          &copy; Not at all protected by copyright. No rights reserved.
-        </p>
-        <div className="ml-4">
-          <a
-            href="https://github.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-600 hover:text-gray-900"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="https://linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-600 hover:text-gray-900 ml-2"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-        </div>
+    <footer className="bg-black p-2 fixed inset-x-0 bottom-0 text-center">
+      <p className="text-green-400"> © Not at all protected by copyright. No rights reserved.</p>
+      <div className="mt-1">
+        <a href="https://github.com" 
+           target="_blank" 
+           rel="noopener noreferrer" 
+           className="text-green-400 text-xl mx-1 transition-colors duration-300 hover:text-blue-500">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://linkedin.com" 
+           target="_blank" 
+           rel="noopener noreferrer" 
+           className="text-green-400 text-xl mx-1 transition-colors duration-300 hover:text-blue-500">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
       </div>
     </footer>
   );
 };
-
 export default Footer;
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 import '../tailwind.css';
-import { useState } from 'react';
 
-const Header = (props) => {
-  const [title, setTitle] = useState('Tree!');
-
-  return (
-    <Fragment>
-      <header className="header" >
-        <h1>{title}</h1>
+const Header = () => {
+    return (
+      <header className="bg-black text-green-400 p-2 fixed inset-x-0 top-0">
+        <div className="flex items-center padding">
+          <h1 className="text-md md:text-xl">Tree!</h1>
+          <a href="https://github.com" 
+             target="_blank" 
+             rel="noopener noreferrer" 
+             className="pl-4  text-green-400 text-xl mx-1 transition-colors duration-300 hover:text-blue-500">
+          Read me!
+          <FontAwesomeIcon icon={faGithub} className="ml-2" />
+        </a>
+        </div>
       </header>
-      {/* <div>
-        <img src={icon} alt="icon" />
-      </div> */}
-    </Fragment>
-  );
+    );
 };
 
 export default Header;
