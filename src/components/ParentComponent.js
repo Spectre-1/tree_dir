f01f4faa1254443471269d4c01667743084a672e
Spectre <pgudijan@uci.edu>
2023-05-25 14:22:52 -0700

    parser not working

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -3,21 +3,48 @@ import InputComponent from './InputComponent';
 import OutputComponent from './OutputComponent';
 import { generateTree } from '../utils/generateTree'; // Corrected path
 
-const parseInputToStructure = (input) => {
-  // Replace with your real implementation
-  return {
-    name: input,
-    parent: null,
-    children: []
-  };
-};
+function stringToStructure(input) {
+  let lines = input.split('\n');
+  let root = { name: '.', children: [] };
+  let currentNodes = [root]; // Root node at depth 0
+
+  for (let line of lines) {
+    let depth = Math.floor(line.search(/\S/) / 2); // Find the first non-space character and divide by 2
+    let name = line.trim();
+
+    if (name.length > 0) { // Ignore blank lines
+      let node = { name: name, children: [] };
+      currentNodes[depth].children.push(node); // Add as child to current node at this depth
+      currentNodes[depth + 1] = node; // This node becomes the current node at the next depth
+    }
+  }
+
+  return root;
+}
+
+const input = `
+src
+  assets
+  components
+  Layout
+    Header
+    Footer
+    Mainbody
+  UI
+    Data_Container
+`;
+
+let structure = stringToStructure(input);
+let tree = generateTree(structure);
+console.log(tree);
+
 
 const ParentComponent = () => {
   const [inputValue, setInputValue] = useState('');
   const [parsedTree, setParsedTree] = useState('');
 
   useEffect(() => {
-    const structure = parseInputToStructure(inputValue);
+    const structure = stringToStructure(inputValue);
     const tree = generateTree(structure);
     setParsedTree(tree);
   }, [inputValue]);
